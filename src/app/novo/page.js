7d205c63556347d7d1cfcd6b82bd2eb34fbe0458
CPUNOF <breno.dantassouza.dantas@gmail.com>
2026-04-10 05:50:16 -0300

    Corrigindo cache da página de novos filmes

--- src/app/novo/page.js
+++ src/app/novo/page.js
@@ -87,6 +87,7 @@ export default function NovoFilme() {
       toast.success("Sucesso! O filme foi para a fila de Sugestões.");
       
       setTimeout(() => {
+        router.refresh();
         router.push("/");
       }, 1500);
 
